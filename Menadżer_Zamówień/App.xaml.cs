9e1acf83ce220b4b38c874fc689924b14ad53238
elerayy <adrijaj434@student.polsl.pl>
2021-07-07 20:03:30 +0200

    Dodaj pliki projektów.

---
+++ Menadżer_Zamówień/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Menadżer_Zamówień
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
